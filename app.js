7ae1a5c114feda1b5ea3f753cfe9ffb41486d0e4
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-09-07 14:33:13 +0200

    Add changes to reviewed code

--- app.js
+++ app.js
@@ -24,38 +24,54 @@ const getWord = async () => {
 
   const play = async (word, underScores) => {
     const read = readline.createInterface({ input, output })
-    const numberOfTries = 11
+    const numberOfTries = word.length + 4
+    let lifespan = numberOfTries
+
+    let hasWon = false
+    let guessedLetters = ''
+
     for (let i = 0; i < numberOfTries; i++ ) {
-      const guessRaw = await read.question('Make a letter/word guess: ')
-      const guess = guessRaw.toLowerCase()
-
-      if (word.includes(guess)) {
-        console.log('good guess')
-        const index = word.indexOf(guess)
-        let underScoresArr = underScores.split(' ')
-        underScoresArr[index] = guess
-        underScores = underScoresArr.join(' ')
-        console.log(underScores)
-
-      } else {
-        console.log('bad guess')
+      if (!hasWon) {
+        const guessRaw = await read.question(`\nNumber of tries left: ${lifespan}\nGuessed letters: ${guessedLetters}\nMake a letter/word guess: `)
+        const guess = guessRaw.toLowerCase()
+        guessedLetters += guess + ' '
+        lifespan -= 1
+  
+        for (let j = 0; j < word.length; j++) {
+          if (word[j] === guess) {
+            let underScoresArr = underScores.split(' ')
+            underScoresArr[j] = guess
+            underScores = underScoresArr.join(' ')
+            
+            if (!underScoresArr.includes('_')) {
+              hasWon = true
+              i = numberOfTries
+            }
+          }
+        }
+  
+        console.log('\n' + underScores)
       }
-
     }
+
+    if (hasWon) {
+      console.log('\nCongratulations!\n')
+    } else {
+      console.log('\nYou lost...\n')
+    }
+
     read.close()
   }
 
-// output modul
-const runApp = async () => {
+  // output modul
+  const runApp = async () => {
   let word = await outputWord()
-  console.log(word);
+
   console.log('Welcome to Hangman!')
   console.log('Your word is:')
   const underScores = await genUnderscores(word)
   console.log(underScores)
   play(word, underScores)
-
-  
 }
 
 runApp()
