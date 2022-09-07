5dc495e64a667c0a3f3c7fb33e8a8f3d106da427
Emma Fransson <ef222hr@student.lnu.se>
2022-09-07 11:27:37 +0200

    Add play function

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ import { stdin as input, stdout as output } from 'node:process'
 // Hämta ord modul...
 const getWord = async () => { 
     const res = await fetch('https://api.api-ninjas.com/v1/randomword')
-    const data = await res.json();
-    return data.word;
+    const data = await res.json()
+    return data.word.toLowerCase()
   }
 
   const outputWord = async () => {
     const word = await getWord()
     return word
   }
-
 
   const genUnderscores = async (word) => {
     let underscores = ''
@@ -23,20 +22,41 @@ const getWord = async () => {
     return underscores
   }
 
+  const play = async (word, underScores) => {
+    const read = readline.createInterface({ input, output })
+    const numberOfTries = 11
+    for (let i = 0; i < numberOfTries; i++ ) {
+      const guessRaw = await read.question('Make a letter/word guess: ')
+      const guess = guessRaw.toLowerCase()
+
+      if (word.includes(guess)) {
+        console.log('good guess')
+        const index = word.indexOf(guess)
+        let underScoresArr = underScores.split(' ')
+        underScoresArr[index] = guess
+        underScores = underScoresArr.join(' ')
+        console.log(underScores)
+
+      } else {
+        console.log('bad guess')
+      }
+
+    }
+    read.close()
+  }
 
 // output modul
 const runApp = async () => {
-  const word = await outputWord()
+  let word = await outputWord()
   console.log(word);
   console.log('Welcome to Hangman!')
   console.log('Your word is:')
-  const uS = await genUnderscores(word)
-  console.log(uS);
+  const underScores = await genUnderscores(word)
+  console.log(underScores)
+  play(word, underScores)
 
-  const read = readline.createInterface({ input, output });
-  const answer = await read.question('What do you think of Node.js? ');
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  read.close();
+  // console.log(`Thank you for your valuable feedback: ${answer}`)
+  
 }
 
 
